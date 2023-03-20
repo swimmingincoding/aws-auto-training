91e711d2f5f0812686b3ecb8417a3c5dd1ee1862
김태균(KIM Teagyun)/Data Intelligence팀/SK <10347@skcc.com>
2023-03-20 16:14:38 +0900

    Modify main.py to receive arguments

--- train/main.py
+++ train/main.py
@@ -1,26 +1,39 @@
+import argparse
 import os
 import sagemaker
 from sagemaker import get_execution_role
 from sagemaker.pytorch import PyTorch
 
-# Set up the Environment
-sagemaker_session = sagemaker.Session()
+def main(args):
+    # Set up the Environment
+    sagemaker_session = sagemaker.Session()
 
-# IAM role setting
-role = get_execution_role()
+    # IAM role setting
+    role = get_execution_role()
 
-# Create a training job using the PyTorch Estimator
-estimator = PyTorch(entry_point="train.py",
-                    role=role,
-                    framework_version="1.8.0",
-                    py_version="py3",
-                    train_instance_count=1,
-                    train_instance_type="ml.c5.xlarge",
-                    hyperparameters={
-                        "epochs": 5,
-                        "backend": "gloo"
-                    })
+    # Create a training job using the PyTorch Estimator
+    estimator = PyTorch(entry_point="train.py",
+                        role=role,
+                        framework_version="1.8.0",
+                        py_version="py3",
+                        train_instance_count=1,
+                        train_instance_type="ml.c5.xlarge", # ml.p2.xlarge
+                        hyperparameters={
+                            "epochs": args.epochs,
+                            "backend": "gloo"
+                        })
 
-# Calling `fit`
-data_path = "s3://sagemaker-pytorch-metal/data/"
-estimator.fit({"train": f"{data_path}"})
+    # Calling `fit`
+    estimator.fit({"train": f"{args.data_dir}"})
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    # Data and model parameters
+    parser.add_argument('--epochs', type=int, default=5, metavar='N',
+                        help='number of epochs to train (default: 5)')
+    
+    # Container environment
+    parser.add_argument('--data-dir', type=str, default="s3://sagemaker-pytorch-metal/data/")
+
+    main(parser.parse_args())
